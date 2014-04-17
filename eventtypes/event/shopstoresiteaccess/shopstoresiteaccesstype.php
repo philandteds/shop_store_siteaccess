6f6b48b315a848a35e67e40f3972e66f9b343ac2
Serhey Dolgushev <dolgushev.serhey@gmail.com>
2014-04-17 21:00:00 +0300

    do not updated siteaccess when current order is paid

--- eventtypes/event/shopstoresiteaccess/shopstoresiteaccesstype.php
+++ eventtypes/event/shopstoresiteaccess/shopstoresiteaccesstype.php
@@ -1,86 +1,96 @@
 <?php
+
 /**
  * @package ShopStoreSiteaccess
  * @author  Serhey Dolgushev <dolgushev.serhey@gmail.com>
  * @date    18 Dec 2012
- **/
+ * */
+class shopStoreSiteaccessType extends eZWorkflowEventType {
 
-class shopStoreSiteaccessType extends eZWorkflowEventType
-{
-	const TYPE_ID = 'shopstoresiteaccess';
+    const TYPE_ID = 'shopstoresiteaccess';
 
-	public function __construct() {
-		$this->eZWorkflowEventType(
-			self::TYPE_ID,
-			ezpI18n::tr( 'extension/shop_store_siteaccess', 'Store used siteaccess' )
-		);
-		$this->setTriggerTypes(
-			array(
-				'shop' => array(
-					'confirmorder' => array( 'before' ),
-					'checkout'     => array( 'after' )
-				)
-			)
-		);
-	}
+    public function __construct() {
+        $this->eZWorkflowEventType(
+            self::TYPE_ID, ezpI18n::tr( 'extension/shop_store_siteaccess', 'Store used siteaccess' )
+        );
+        $this->setTriggerTypes(
+            array(
+                'shop' => array(
+                    'confirmorder' => array( 'before' ),
+                    'checkout'     => array( 'after' )
+                )
+            )
+        );
+    }
 
-	public function execute( $process, $event ) {
-		$parameters = $process->attribute( 'parameter_list' );
-		if( isset( $parameters['order_id'] ) === false ) {
-			return eZWorkflowEventType::STATUS_ACCEPTED;
-		}
-		$order = eZOrder::fetch( $parameters['order_id'] );
-		if( $order instanceof eZOrder === false ) {
-			return eZWorkflowEventType::STATUS_ACCEPTED;
-		}
+    public function execute( $process, $event ) {
+        $parameters = $process->attribute( 'parameter_list' );
+        if( isset( $parameters['order_id'] ) === false ) {
+            return eZWorkflowEventType::STATUS_ACCEPTED;
+        }
+        $order = eZOrder::fetch( $parameters['order_id'] );
+        if( $order instanceof eZOrder === false ) {
+            return eZWorkflowEventType::STATUS_ACCEPTED;
+        }
 
-		$check = eZOrderItem::fetchListByType( $order->attribute( 'id' ), 'siteaccess' );
-		if( count( $check ) > 0 && $order->attribute( 'is_temporary' ) ) {
-			return eZWorkflowEventType::STATUS_ACCEPTED;
-		}
+        $check = eZOrderItem::fetchListByType( $order->attribute( 'id' ), 'siteaccess' );
+        if( count( $check ) > 0 && $order->attribute( 'is_temporary' ) ) {
+            return eZWorkflowEventType::STATUS_ACCEPTED;
+        }
 
-		if( count( $check ) > 0 ) {
-			foreach( $check as $item ) {
-				$item->remove();
-			}
-		}
+        if( count( $check ) > 0 ) {
+            foreach( $check as $item ) {
+                $item->remove();
+            }
+        }
 
-		$orderItem = new eZOrderItem(
-			array(
-				'order_id'        => $order->attribute( 'id' ),
-				'description'     => $GLOBALS['eZCurrentAccess']['name'],
-				'price'           => 0,
-				'type'            => 'siteaccess',
-				'vat_is_included' => true,
-				'vat_type_id'     => 1
-			)
-		);
-		$orderItem->store();
+        $orderItem = new eZOrderItem(
+            array(
+            'order_id'        => $order->attribute( 'id' ),
+            'description'     => $GLOBALS['eZCurrentAccess']['name'],
+            'price'           => 0,
+            'type'            => 'siteaccess',
+            'vat_is_included' => true,
+            'vat_type_id'     => 1
+            )
+        );
+        $orderItem->store();
 
-		return eZWorkflowType::STATUS_ACCEPTED;
-	}
+        return eZWorkflowType::STATUS_ACCEPTED;
+    }
 
-	public static function fix( $output ) {
-		$basket = eZBasket::currentBasket();
-		if( $basket instanceof eZBasket === false ) {
-			return $output;
-		}
+    public static function fix( $output ) {
+        // Order is complete if there is no current basket
+        $basket = eZBasket::currentBasket();
+        if( $basket instanceof eZBasket === false ) {
+            return $output;
+        }
 
-		$items  = eZOrderItem::fetchListByType( $basket->attribute( 'order_id' ), 'siteaccess' );
-		if( count( $items ) > 0 ) {
-			$item = $items[0];
-			if( $item->attribute( 'description' ) != $GLOBALS['eZCurrentAccess']['name'] ) {
-				$item->setAttribute( 'description', $GLOBALS['eZCurrentAccess']['name'] );
-				$item->store();
-			}
-		}
+        // Check if current order is paid (in some rare cases order is completed, but it still has the basket)
+        $db = eZDB::instance();
+        $q  = 'SELECT o.id
+            FROM ezorder o
+            LEFT JOIN xrowpaymentobject p ON p.order_id = o.id
+            WHERE o.id = ' . $basket->attribute( 'order_id' ) . ' AND p.status = 1';
+        if( count( $db->arrayQuery( $q ) ) > 0 ) {
+            return $output;
+        }
 
-		return $output;
-	}
+        $items = eZOrderItem::fetchListByType( $basket->attribute( 'order_id' ), 'siteaccess' );
+        if( count( $items ) > 0 ) {
+            $item = $items[0];
+            if( $item->attribute( 'description' ) != $GLOBALS['eZCurrentAccess']['name'] ) {
+                $item->setAttribute( 'description', $GLOBALS['eZCurrentAccess']['name'] );
+                $item->store();
+            }
+        }
+
+        return $output;
+    }
+
 }
 
 eZWorkflowEventType::registerEventType(
-	shopStoreSiteaccessType::TYPE_ID,
-	'shopStoreSiteaccessType'
+    shopStoreSiteaccessType::TYPE_ID, 'shopStoreSiteaccessType'
 );
 ?>
